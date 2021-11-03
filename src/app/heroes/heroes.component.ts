15a03c47203e57da2cc4d4e57b313becec8aebc2
seattleriot <seattleriot@hotmail.com>
2021-11-03 15:55:20 -0700

    updated Readme

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,60 +1,83 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
-import { ActivatedRoute } from '@angular/router';
-import { AppInsightsService } from '../app-insights.service';
-import { query, trigger, state, style, transition, stagger, animate, group } from '@angular/animations'; 
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
+import { ActivatedRoute } from "@angular/router";
+import { AppInsightsService } from "../app-insights.service";
+import {
+  query,
+  trigger,
+  state,
+  style,
+  transition,
+  stagger,
+  animate,
+  group,
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"],
   animations: [
-    trigger('flyInOut', [
-      state('in', style({width: 120, transform: 'translateX(0)', opacity: 1})),
-      transition('void => *', [
-        style({width: 10, transform: 'translateX(50px)', opacity: 0}),
+    trigger("flyInOut", [
+      state(
+        "in",
+        style({ width: 120, transform: "translateX(0)", opacity: 1 })
+      ),
+      transition("void => *", [
+        style({ width: 10, transform: "translateX(50px)", opacity: 0 }),
         group([
-          animate('0.3s 0.1s ease', style({
-            transform: 'translateX(0)',
-            width: 120
-          })),
-          animate('0.3s ease', style({
-            opacity: 1
-          }))
-        ])
+          animate(
+            "0.3s 0.1s ease",
+            style({
+              transform: "translateX(0)",
+              width: 120,
+            })
+          ),
+          animate(
+            "0.3s ease",
+            style({
+              opacity: 1,
+            })
+          ),
+        ]),
       ]),
-      transition('* => void', [
+      transition("* => void", [
         group([
-          animate('0.3s ease', style({
-            transform: 'translateX(50px)',
-            width: 10
-          })),
-          animate('0.3s 0.2s ease', style({
-            opacity: 0
-          }))
-        ])
-      ])
-    ])
-  ]
+          animate(
+            "0.3s ease",
+            style({
+              transform: "translateX(50px)",
+              width: 10,
+            })
+          ),
+          animate(
+            "0.3s 0.2s ease",
+            style({
+              opacity: 0,
+            })
+          ),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
+  inputItem: string;
 
   constructor(
-    private appInsightsService: AppInsightsService, 
+    private appInsightsService: AppInsightsService,
     private heroService: HeroService,
-    private route: ActivatedRoute) {}
-  
+    private route: ActivatedRoute
+  ) {}
+
   ngOnInit() {
     this.getHeroes();
   }
 
-
   getHeroes(): void {
-    this.appInsightsService.logPageEvent('getHeroes - heroes component');;
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    this.appInsightsService.logPageEvent("getHeroes - heroes component");
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
